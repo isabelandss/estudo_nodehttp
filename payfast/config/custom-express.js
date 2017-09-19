3abc97a70ed71c2ff947a38b2997be953505b00b
Isabela Neri dos Santos <isabela.santos@fcamara.com.br>
2017-09-19 10:57:33 -0300

    Aula 2-6

--- payfast/config/custom-express.js
+++ payfast/config/custom-express.js
@@ -1,12 +1,16 @@
 var express = require('express');
 var consign = require('consign');
 var bodyParser = require('body-parser');
+var expressvalidator = require('express-validator');
 
 module.exports = function() {
     var app = express();
 
+    //ensinando que os dados serão passados pela url do navegador
     app.use(bodyParser.urlencoded({extended: true}));
+    //ensinando que os dados serão em json
     app.use(bodyParser.json());
+    app.use(expressvalidator());
 
     //passando o conhecimento, mostrando a existência da pasta controllers
     consign()
--- payfast/controllers/pagamentos.js
+++ payfast/controllers/pagamentos.js
@@ -7,6 +7,15 @@ module.exports = function (app) {
     });
 
     app.post('/pagamentos/pagamento', function(req, res) {
+        req.assert("forma_de_pagamento", "Forma de pagamento eh obrigatorio.").notEmpty();
+        req.assert("valor", "Valor eh obrigatorio e deve ser decimal").notEmpty().isFloat();
+        var erros = req.validationErrors();
+        if(erros) {
+            console.log("Erros de validacao encontrados");
+            res.status(500).send(erros);
+            return;
+        }
+
         var pagamento = req.body;
         console.log('Processando uma requisisção de um novo pagamento');
         pagamento.status = 'CRIADO';
@@ -16,8 +25,14 @@ module.exports = function (app) {
         var pagamentoDAO = new app.persistencia.PagamentosDAO(connection);
 
         pagamentoDAO.salva(pagamento, function(erro, resultado) {
-            console.log("PAGAMENTO CRIADO");
-            res.json(pagamento);
+            if(erro) {
+                console.log("ERRO AO INSERIR NO BANCO " + erro);
+                res.status(400).send(erro);
+            } else {
+                console.log("PAGAMENTO CRIADO");
+                res.location("/pagamentos/pagamento/" + resultado.insertId);
+                res.status(201).json(pagamento);
+            }
         });
     });
 }
